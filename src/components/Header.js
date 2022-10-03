c5baa6eda39e4d38b96b91cfa2953fa7adeed187
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-03 20:11:48 -0300

    requisito 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -21,14 +22,23 @@ class Header extends Component {
 
   render() {
     const { isLoading, userName } = this.state;
-    if (isLoading === true) return <Loading />;
 
     return (
       <header data-testid="header-component">
         <p> PLACEHOLDER HEADER </p>
 
+        <NavLink to="/search" data-testid="link-to-search">
+          Pesquisa
+        </NavLink>
+        <NavLink to="/favorites" data-testid="link-to-favorites">
+          Favoritas
+        </NavLink>
+        <NavLink to="/profile" data-testid="link-to-profile">
+          Perfil
+        </NavLink>
+
         <span data-testid="header-user-name">
-          {userName}
+          {isLoading ? <Loading /> : userName}
         </span>
       </header>
     );
